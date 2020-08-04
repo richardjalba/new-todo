ff27888692db4de2adac675037c9d038f9348a19
Richard Alba <64925670+richardjalba@users.noreply.github.com>
2020-08-04 12:00:20 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const todoInputEl = document.querySelector(".todo__input");
+const todoListEl = document.querySelector(".todo__list");
+const todoItemEls = document.querySelectorAll(".todo__item");
+
+function addListItem() {
+  todoInputEl.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      let newListItem = createListItem(todoInputEl.value);
+      todoListEl.appendChild(newListItem);
+      todoListEl.insertBefore(newListItem, todoListEl.childNodes[0]);
+      todoInputEl.value = "";
+    }
+  });
+}
+
+// function toggleDone() {
+//   for (let elem of todoItemEls) {
+//     elem.addEventListener("click", function () {
+//       elem.classList.toggle("done");
+//     });
+//   }
+// }
+
+function toggleDone() {
+  todoListEl.addEventListener("click", function (event) {
+    if (event.target.classList.contains("todo__item")) {
+      event.target.classList.toggle("done");
+    }
+  });
+}
+
+function createListItem(text) {
+  const newListElement = document.createElement("li");
+  newListElement.textContent = text;
+  newListElement.setAttribute("class", "todo__item");
+  return newListElement;
+}
+
+toggleDone();
+addListItem();
